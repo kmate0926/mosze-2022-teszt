89dbec78e860130afa768244d8ec8fc2c1b1be6d
kmate0926 <71819840+kmate0926@users.noreply.github.com>
2022-09-13 15:35:06 +0200

    Update main.cpp
    javitva

--- main.cpp
+++ main.cpp
@@ -4,23 +4,23 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; //N_ELEMENTS
-    std::cout << '1-100 ertekek duplazasa' //;
-    for (int i = 0;) //nincs megadva az iterálás vége, milyen lépésközzel
+    int *b = new int[N_ELEMENTS]; //N_ELEMENTS
+    std::cout << '1-100 ertekek duplazasa'; //;
+    for (int i = 0;i<N_ELEMENTS;i++) //nincs megadva az iterálás vége, milyen lépésközzel
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++) //egyszer fut le
+    for (int i = 0; i <N_ELEMENTS; i++) //egyszer fut le
     {
-        std::cout << "Ertek:" //; nincs érték kiiratás
+        std::cout << "Ertek:" <<b[i]<<std::endl; //; nincs érték kiiratás
     }    
     std::cout << "Atlag szamitasa: " << std::endl; 
-    int atlag; //double 
-    for (int i = 0; i < N_ELEMENTS, i++)// N_ELEMENTS utáni vessző helyett ;
+    double atlag; //double 
+    for (int i = 0; i < N_ELEMENTS; i++)// N_ELEMENTS utáni vessző helyett ;
     {
-        atlag += b[i] //;
+        atlag += b[i]; //;
     }
-    atlag /= N_ELEMENTS; //atlag=atlag/N_ELEMENTS
+    atlag =atlag/N_ELEMENTS; //atlag=atlag/N_ELEMENTS
     std::cout << "Atlag: " << atlag << std::endl;
     return 0;
 }
